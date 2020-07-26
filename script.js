ced17eb4f4ad71cf88d1f217307548c975616f0e
olgakreits <42476253+olgakreits@users.noreply.github.com>
2020-07-26 20:48:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const accordion1 = new accComponent({
+const accordeon1 = new accComponent({
   panels: '.block_panel',
   hidePanels: '.hidden_panel'
 });
 
-const accordion2 = new accComponent({
+const accordeon2 = new accComponent({
   panels: '.block_panel2',
   hidePanels: '.hidden_panel2',
-  accordion: '.accordion2'
+  accordeon: '.accordeon2'
 });
 
-accordion1.vertical_auto();
-accordion2.horizontal_auto();
+accordeon1.vertical_auto();
+accordeon2.horizontal_auto();
